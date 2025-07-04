f1e2b7894a9f4e45e00e3e0e84c557c3111a2425
Cosimo0C <cosimocrupi01@gmail.com>
2025-07-04 17:30:15 +0200

    almost done classes and DAO

--- src/main/java/cosimocrupi/entities/Catalogo.java
+++ src/main/java/cosimocrupi/entities/Catalogo.java
@@ -1,20 +1,23 @@
 package cosimocrupi.entities;
 
+import jakarta.persistence.Id;
+
 import java.util.UUID;
 
 public abstract class Catalogo{
-     protected String isbn;
+
      protected String titolo;
      protected int annoProduzione;
      protected int numeroPagine;
-
+     @Id
+    UUID isbn;
      public Catalogo(String titolo, int annoProduzione, int numeroPagine) {
          this.titolo=titolo;
          this.annoProduzione=annoProduzione;
          this.numeroPagine=numeroPagine;
      }
 
-     public String getIsbn() {
+     public UUID getIsbn() {
          return isbn;
      }
 
